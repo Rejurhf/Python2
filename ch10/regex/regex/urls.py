24256a80704152d5179f98988320f44e2479c754
Rejurhf <mziomek108@gmail.com>
2018-11-19 15:37:27 +0100

    Django Done

--- ch10/regex/regex/urls.py
+++ ch10/regex/regex/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.core.urlresolvers import reverse_lazy
+from entries.views import HomeView, EntryListView, EntryFormView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^entries/$', EntryListView.as_view(), name='entries'),
+    url(r'^entries/insert$', EntryFormView.as_view(), name='insert'),
+    url(r'^login/$',
+        auth_views.login,
+        kwargs={'template_name': 'admin/login.html'},
+        name='login'),
+    url(r'^logout/$',
+        auth_views.logout,
+        kwargs={'next_page': reverse_lazy('home')},
+        name='logout'),
+    url(r'^$', HomeView.as_view(), name='home'),
+] 
