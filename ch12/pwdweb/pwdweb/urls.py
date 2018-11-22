71894e651234ff677db79d4de2ca3c2e94e0fcbc
Rejurhf <mziomek108@gmail.com>
2018-11-22 21:29:33 +0100

    Django In progress

--- ch12/pwdweb/pwdweb/urls.py
+++ ch12/pwdweb/pwdweb/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from records import urls as records_url
+from records.views import HomeView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),    
+    url(r'^records/', include(records_url, namespace='records')),
+    url(r'^$', HomeView.as_view(), name='home'),
 ]
